c04046614f0d96f602ddfaf48181e0a7a59166dc
GusJTT <gturpot@unsa.edu.pe>
2024-12-19 03:04:31 -0500

    Implementando filtrado por galerias * Se ha creado una lista desplegable para seleccionar la galería * Se ha creado un método en PictureViewModel para obtener las galerías desde las pinturas obtenidas * Se ha creado un método en PictureViewModel para obtener las pinturas dado un id de galería

--- ViewModels/PictureViewModel.swift
+++ ViewModels/PictureViewModel.swift
@@ -8,17 +8,20 @@
 import Foundation
 import Combine
 
+struct GalleryOption : Hashable{
+    var id: Int
+    var name: String
+}
+
 class PictureViewModel: ObservableObject {
 
     @Published var pictures: [Pictures] = [] 
     @Published var errorMessage: String? = nil
     @Published var isLoading = false
-    @Published var galleries: [String] = []
+    @Published var galleries: [GalleryOption] = []
     
     private var repository: DataRepository
     private var cancellables = Set<AnyCancellable>()
-    
-    
     
     init(repository: DataRepository = Repository()) {
         self.repository = repository
@@ -47,14 +50,20 @@ class PictureViewModel: ObservableObject {
     }
     func fetchGalleries(){
         if !pictures.isEmpty {
-            galleries = Array(Set(pictures.map { "Galería \($0.roomId.toRoman)"})).sorted()
-            print("Galerías cargadas: \(galleries)")  // Verificar si las galerías están siendo cargadas
+            // Crear un conjunto de objetos GalleryOption
+            galleries = Array(Set(pictures.map { GalleryOption(id: $0.roomId, name: "Galería \($0.roomId.toRoman)") })).sorted {
+                    $0.id < $1.id // Ordena las galerías por el id
+            }
+            print("Galerías cargadas: \(galleries.map { $0.name })")
         } else {
             print("No hay pinturas cargadas aún.")
         }
     }
     func getPictureById(id: Int) -> Pictures? {
         return pictures.first { $0.id == id }
+    }
+    func filteredPictures(forGalleryId galleryId: Int) -> [Pictures] {
+        return pictures.filter { $0.roomId == galleryId }
     }
 }
 
--- Views/GalleryListView.swift
+++ Views/GalleryListView.swift
@@ -9,67 +9,124 @@ import SwiftUI
 
 struct GalleryListView: View {
     @StateObject private var pictureViewModel = PictureViewModel()
-    @State private var selectedOption = "Opción 1"
-    let options = ["Opción 1", "Opción 2", "Opción 3"]
+    @State var selection1: String? = nil
+    @State var selectedGalleryId: Int? = nil
     
     var body: some View {
         VStack {
+            DropDownPicker(
+                selectedGalleryId: $selectedGalleryId,
+                options: pictureViewModel.galleries
+            )
+            .padding(.bottom, 10)
+            .padding(.horizontal, 15)
             ScrollView {
-                if pictureViewModel.isLoading {
-                    ProgressView()
-                }
-                else if let errorMessage = pictureViewModel.errorMessage {
-                    Text(errorMessage)
-                        .foregroundStyle(Color .red)
-                } else {
-                    /*LazyVGrid(columns: [GridItem(.adaptive(minimum: 160, maximum: 160))], spacing: 15) {
-                        ForEach(pictureViewModel.galleries, id: \.self) { gallery in
-                            Button(action: {
-                                //selectedGallery = gallery
-                            }) {
-                                Text(gallery)
-                                    .padding()
-                                    .background(Color.blue)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(8)
-                            }
-                        }
-                    }*/
-                    VStack {
-                        Text("Seleccionado: \(selectedOption)")
-                        .font(.headline)
-                                
-                        Picker("Selecciona una opción", selection: $selectedOption) {
-                                ForEach(options, id: \.self) { option in
-                                    Text(option)
-                                }
-                            }
-                            .pickerStyle(MenuPickerStyle()) // Estilo de lista desplegable
-                            .padding()
-                    }
-                    
-                    // Mostrar pinturas si una galería está seleccionada
-                    /*if let selectedGallery = selectedGallery {
-                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 160, maximum: 160))], spacing: 15) {
-                            ForEach(pictureViewModel.paintings(for: selectedGallery)) { picture in
-                                PictureRowView(picture: picture)
-                            }
-                        }
-                    }*/
-                }
-                                    
+                PaintingListView()
             }
-            /*.fullScreenCover(item: $selectedPicture) { picture in
-                PictureDetailView(picture: picture, onClose: {
-                    selectedPicture = nil
-                })
-            }              //.navigationTitle("Pictures")*/
         }
         .onAppear {
-            pictureViewModel.fetchPictures()  // Llamamos a fetchPictures
+            pictureViewModel.fetchPictures()
         }
         .onChange(of: pictureViewModel.pictures) { _ in
-            pictureViewModel.fetchGalleries() // Llamamos a fetchGalleries manualmente cuando pictures cambia
+            pictureViewModel.fetchGalleries()
         }
+    }
+}
+
+enum DropDownPickerState {
+    case top
+    case bottom
+}
+
+struct DropDownPicker: View {
+    
+    @State var selection: String?
+    @Binding var selectedGalleryId: Int?
+    var state: DropDownPickerState = .bottom
+    var options: [GalleryOption]
+    var maxWidth: CGFloat = 180
+    
+    @State var showDropdown = false
+    
+    @SceneStorage("drop_down_zindex") private var index = 1000.0
+    @State var zindex = 1000.0
+    
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            
+            VStack(spacing: 0) {
+                if state == .top && showDropdown {
+                    OptionsView()
+                }
+                HStack {
+                    Text(selection == nil ? "Select" : selection!)
+                        .foregroundColor(selection != nil ? .black : .gray)
+                    Spacer(minLength: 0)
+                    
+                    Image(systemName: state == .top ? "chevron.up" : "chevron.down")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                        .rotationEffect(.degrees((showDropdown ? -180 : 0)))
+                }
+                .padding(.horizontal, 15)
+                //.frame(width: 180, height: 50)
+                .frame(height: 50)
+                .background(.white)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    index += 1
+                    zindex = index
+                    withAnimation(.linear) {
+                        showDropdown.toggle()
+                    }
+                }
+                .zIndex(10)
+                
+                if state == .bottom && showDropdown {
+                    OptionsView()
+                }
+            }
+            .clipped()
+            .background(.white)
+            .cornerRadius(10)
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.gray)
+            }
+            .frame(height: size.height, alignment: state == .top ? .bottom : .top)
+            
+        }
+        //.frame(width: maxWidth, height: 50)
+        .frame(height: 50)
+        .zIndex(zindex)
+    }
+    
+    
+    func OptionsView() -> some View {
+        VStack(spacing: 0) {
+            ForEach(options, id: \.self) { option in
+                HStack {
+                    Text(option.name)
+                    Spacer()
+                    Image(systemName: "checkmark")
+                        .opacity(selection == option.name ? 1 : 0)
+                }
+                .foregroundStyle(selection == option.name ? Color.primary : Color.gray)
+                .animation(.none, value: selection)
+                .frame(height: 40)
+                .contentShape(Rectangle())
+                .padding(.horizontal, 15)
+                .onTapGesture {
+                    withAnimation(.linear) {
+                        selection = option.name
+                        selectedGalleryId = option.id
+                        showDropdown.toggle()
+                    }
+                }
+            }
+        }
+        .transition(.move(edge: state == .top ? .bottom : .top))
+        .zIndex(1)
     }
 }
