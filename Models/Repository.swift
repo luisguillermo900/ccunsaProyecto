da68084600244dbb715fd5b9d82272afe2fb2cd2
luisguillermo900 <83891743+luisguillermo900@users.noreply.github.com>
2024-12-19 11:55:17 -0800

    comentario sobre el url

--- Models/Repository.swift
+++ Models/Repository.swift
@@ -27,7 +27,11 @@ extension DataRepository {
 
 struct Repository: DataRepository {
     var url: URL {
-        URL(string: "https://ccunsa-77a2c-default-rtdb.firebaseio.com/ccunsa/pictures.json")!
+        //RL(string: "https://ccunsa-77a2c-default-rtdb.firebaseio.com/ccunsa/pictures.json")!
+        URL(string: "http://10.50.33.109:3000/api/v1/pictures")! //colocar la ip pública de la máquina que se
+        //está ejecutando el backend
+        
+        // http://localhost:3000/api/v1/pictures
     }
 }
 
